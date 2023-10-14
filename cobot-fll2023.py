82e6fd846e19b5cc87620316aba3f1d1fe8f9641
SAmUel-DEvIL <129573518+SAmUel-DEvIL@users.noreply.github.com>
2023-10-14 17:18:14 +1300

    Update cobot-fll2023.py
    Basic code

--- cobot-fll2023.py
+++ cobot-fll2023.py
@@ -1,18 +1,21 @@
-from hub import port
-import runloop
+from hub import motion_sensor, port
 import motor_pair
+motor_pair.pair(motor_pair.PAIR_1, port.E, port.F)
+add = 30
+def move_forward():
+    turn = (0 + motion_sensor.tilt_angles()[0])/2
+    motor_pair.move(motor_pair.PAIR_1, int(turn), velocity=100, acceleration=100)
+    print(motion_sensor.tilt_angles()[0])
 
-async def main():
-    # Pair motors on port A and B
-    motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
+def turn(amount):
+    if amount < 0:
+        while motion_sensor.tilt_angles()[0] > amount:
+            motor_pair.move(motor_pair.PAIR_1, 100, velocity=100)
+        motor_pair.stop(motor_pair.PAIR_1)
+    else:
+        while motion_sensor.tilt_angles()[0] < amount:
+            motor_pair.move(motor_pair.PAIR_1, -100, velocity=100)
+        motor_pair.stop(motor_pair.PAIR_1)
 
-    # Move straight at default velocity for 360 degrees
-    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 360, 1000, 1000)
-
-    # Turn right for 180 degrees
-    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 180, 0, 1000)
-
-    # Perform tank turn for 720 degrees
-    await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 720, 1000, -1000)
-
-runloop.run(main())
+while True:
+    turn(900-add)
